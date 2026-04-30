c27fb4a21df95c6c34732320d7660350d179efde
Igor <igor.teofilovic144@gmail.com>
2026-04-30 10:42:01 +0200

    Inicijalizacija

---
+++ Pokrivenost/src/main/java/edu/faculty/pokrivenost/Pokrivenost.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.faculty.pokrivenost;
+
+/**
+ *
+ * @author Igor
+ */
+public class Pokrivenost {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
